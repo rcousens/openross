3180e62fddf8e6a117539cfcdd6ee366747123a2
Ross Cousens <ross@hatesmail.com>
2014-07-15 00:15:50 +1000

    Dashboard and login with bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new OpenRoss\Bundle\UserBundle\OpenRossUserBundle(),
             new \FOS\UserBundle\FOSUserBundle(),
+            new OpenRoss\Bundle\DashboardBundle\OpenRossDashboardBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/OpenRoss/Bundle/UserBundle/OpenRossUserBundle.php
+++ src/OpenRoss/Bundle/UserBundle/OpenRossUserBundle.php
@@ -6,4 +6,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OpenRossUserBundle extends Bundle
 {
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
