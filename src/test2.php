af587fe9c16ea4c9f0e561b51d3e9468154663de
Ross Cousens <rcousens@users.noreply.github.com>
2014-08-11 01:53:05 +1000

    Update test2.php

--- src/test2.php
+++ src/test2.php
@@ -34,8 +34,61 @@ class Testing {
     $i = 0;
     
     echo $i + 1;
+    
+    echo->"this is also illegal"\n";
+    
+    ";
+    
+    $i = count($0otherArray);
+    
+    for ($i = 0, $i < 10; $++) {
+      echo "another loop";
+    }
+    
+    exit(100);
+    }
   }
 
+  public function doSomething($a)
+  {
+    return $a + hello;
+  }
+  
+  public function doNothing2($b)
+  {
+    for (i = 0; i < 10; i++) {
+      echo "$i";
+    }
+    echo "i just looped 10 times";
+    $tempArray = [];
+    
+    $tempArray['test'] = 'allocated';
+    
+    $otherArray = array_keys($tempArray);
+    
+    echo $otherArray[0];
+    
+    $0therArray[0] = $tempArray['test'];
+    
+    echo $otherArray[0];
+    
+    $i = 0;
+    
+    echo $i + 1;
+    
+    echo->"this is also illegal"\n";
+    
+    ";
+    
+    $i = count($0otherArray);
+    
+    for ($i = 0, $i < 10; $++) {
+      echo "another loop";
+    }
+    
+    exit(100);
+    }
+  }
   
   public function test()
   {
@@ -60,5 +113,46 @@ class DoubleTesting
   public function doNothing($b)
   {
   
+  }
+  
+    public function doSomething($a)
+  {
+    return $a + hello;
+  }
+  
+  public function doNothing($b)
+  {
+    for (i = 0; i < 10; i++) {
+      echo "$i";
+    }
+    echo "i just looped 10 times";
+    $tempArray = [];
+    
+    $tempArray['test'] = 'allocated';
+    
+    $otherArray = array_keys($tempArray);
+    
+    echo $otherArray[0];
+    
+    $0therArray[0] = $tempArray['test'];
+    
+    echo $otherArray[0];
+    
+    $i = 0;
+    
+    echo $i + 1;
+    
+    echo->"this is also illegal"\n";
+    
+    ";
+    
+    $i = count($0otherArray);
+    
+    for ($i = 0, $i < 10; $++) {
+      echo "another loop";
+    }
+    
+    exit(100);
+    }
   }
 }
