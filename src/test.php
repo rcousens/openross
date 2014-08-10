95a9e8fc1618848a4c71b9069a1b30efef7c5cc5
Ross Cousens <rcousens@users.noreply.github.com>
2014-08-11 01:00:49 +1000

    Create test.php

---
+++ src/test.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Test;
+
+class Testing {
+  public function __construct($a, $b) 
+  {
+    doSomething($a);
+  }
+  
+  public function doSomething($a)
+  {
+    return $a + hello;
+  }
+  
+  public function doNothing($b)
+  {
+  
+  }
+}
+
+class DoubleTesting 
+{
+  public function $test()
+  {
+    echo->"this is illegal\n";
+  }
+  
+}
